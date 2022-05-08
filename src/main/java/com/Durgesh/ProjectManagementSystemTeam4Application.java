2682b29a6b7e1665df9ef0ec37ddc973b4edad0d
Shshikantg <javadeveloper8830676260@gmail.com>
2022-05-08 18:10:24 +0530

    shshikant gholve created

---
+++ src/main/java/com/Durgesh/ProjectManagementSystemTeam4Application.java
@@ -0,0 +1,13 @@
+package com.Durgesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementSystemTeam4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementSystemTeam4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Durgesh/ProjectManagementSystemTeam4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Durgesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementSystemTeam4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
